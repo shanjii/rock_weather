4492f8cdbe206eee7588a4ad4ded529880329d4d
Victor <victor.galves@live.com>
2024-07-18 14:03:26 -0300

    Added code comments

--- lib/core/utils/convertions.dart
+++ lib/core/utils/convertions.dart
@@ -9,15 +9,17 @@ class Convertions {
     String format = 'dd/MM/yyyy, HH:mm',
     required int dt,
   }) {
-    DateTime date = DateTime.fromMillisecondsSinceEpoch(dt * 1000);
+    DateTime date = unixToDateTime(dt);
     return DateFormat(format).format(date);
   }
 
+  ///Receives a Unix timestamp and returns it as a DateTime.
   static DateTime unixToDateTime(int dt) {
+    //Multiply the dt by 1000 for a valid return since the timestamp is received in seconds
     return DateTime.fromMillisecondsSinceEpoch(dt * 1000);
   }
 
-  ///Receives a [String] and removes its diacritics.
+  ///Receives a [String] and removes its diacritics and capitalization.
   static String simplifyString(String string) {
     var e = 'ÀÁÂÃÄÅàáâãäåÒÓÔÕÕÖØòóôõöøÈÉÊËèéêëðÇçÐÌÍÎÏìíîïÙÚÛÜùúûüÑñŠšŸÿýŽž';
     var x = 'AAAAAAaaaaaaOOOOOOOooooooEEEEeeeeeCcDIIIIiiiiUUUUuuuuNnSsYyyZz';
